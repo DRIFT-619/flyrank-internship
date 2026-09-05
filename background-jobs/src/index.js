3f76deb4c5e5e47fd11d62ff4b31efd6908e53bb
Ayush <reignofayush@gmail.com>
2026-09-05 12:32:13 +0530

    background-jobs: Stage 1 - Inngest connected, first function runs

--- background-jobs/src/index.js
+++ background-jobs/src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const { serve } = require('inngest/express');
+const { inngest } = require('./inngest/client');
+const { sayHello } = require('./inngest/functions');
+
 const app = express();
 const PORT = 3000;
 
 app.get('/health', (req, res) => {
   res.json({ status: 'ok' });
 });
+
+app.use('/api/inngest', express.json(), serve({ client: inngest, functions: [sayHello] }));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
